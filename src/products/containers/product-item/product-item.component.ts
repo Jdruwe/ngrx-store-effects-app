e9bf03a254fce620d1683db6c59565d2d5fc1b6e
Jeroen Druwe <druwe.jeroen@gmail.com>
2018-01-28 16:20:00 +0100

    Added topping action

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -1,15 +1,15 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
-import {Store} from "@ngrx/store";
-import {Observable} from "rxjs/Observable";
-import * as fromStore from '../../store';
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/Observable";
+import * as fromStore from "../../store";
 
-import {Pizza} from '../../models/pizza.model';
-import {Topping} from '../../models/topping.model';
+import { Pizza } from "../../models/pizza.model";
+import { Topping } from "../../models/topping.model";
 
 @Component({
-  selector: 'product-item',
-  styleUrls: ['product-item.component.scss'],
+  selector: "product-item",
+  styleUrls: ["product-item.component.scss"],
   template: `
     <div
       class="product-item">
@@ -25,36 +25,29 @@ import {Topping} from '../../models/topping.model';
         </pizza-display>
       </pizza-form>
     </div>
-  `,
+  `
 })
 export class ProductItemComponent implements OnInit {
   pizza$: Observable<Pizza>;
   visualise: Pizza;
   toppings: Topping[];
 
-  constructor(private store: Store<fromStore.ProductState>) {
-  }
+  constructor(private store: Store<fromStore.ProductState>) {}
 
   ngOnInit() {
+    this.store.dispatch(new fromStore.LoadToppings());
     this.pizza$ = this.store.select(fromStore.getSelectedPizza);
   }
 
-  onSelect(event: number[]) {
+  onSelect(event: number[]) {}
 
-  }
+  onCreate(event: Pizza) {}
 
-  onCreate(event: Pizza) {
-
-  }
-
-  onUpdate(event: Pizza) {
-
-  }
+  onUpdate(event: Pizza) {}
 
   onRemove(event: Pizza) {
-    const remove = window.confirm('Are you sure?');
+    const remove = window.confirm("Are you sure?");
     if (remove) {
-
     }
   }
 }
