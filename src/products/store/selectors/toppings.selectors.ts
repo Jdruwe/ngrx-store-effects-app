2a0996b8c4112c51480a34bc7546f2b083bd25c8
Jeroen Druwe <druwe.jeroen@gmail.com>
2018-02-01 08:12:59 +0100

    Added toppings selector

---
+++ src/products/store/selectors/toppings.selectors.ts
@@ -0,0 +1,33 @@
+import { createSelector } from "@ngrx/store";
+
+import * as fromRoot from "../../../app/store";
+import * as fromFeature from "../reducers";
+import * as fromToppings from "../reducers/toppings.reducers";
+
+import { Topping } from "../../models/topping.model";
+import { RouterStateUrl } from "../../../app/store/reducers";
+import * as fromRouter from "@ngrx/router-store";
+
+export const getToppingsState = createSelector(
+  fromFeature.getProductsState,
+  (state: fromFeature.ProductState) => state.toppings
+);
+
+export const getToppingEntities = createSelector(
+  getToppingsState,
+  fromToppings.getToppingEntities
+);
+
+export const getAllToppings = createSelector(getToppingEntities, entities => {
+  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+});
+
+export const getToppingsLoaded = createSelector(
+  getToppingsState,
+  fromToppings.getToppingsLoaded
+);
+
+export const getToppingsLoading = createSelector(
+  getToppingsState,
+  fromToppings.getToppingsLoading
+);
