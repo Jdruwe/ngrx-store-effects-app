18f16d12660972b025bbf01e9329ca0aa380b72c
Jeroen Druwe <druwe.jeroen@gmail.com>
2018-02-06 08:08:50 +0100

    Create pizza added

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -1,25 +1,41 @@
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 
-import {Effect, Actions} from "@ngrx/effects";
-import * as pizzaActions from '../actions/pizzas.action'
+import { Effect, Actions } from "@ngrx/effects";
+import * as pizzaActions from "../actions/pizzas.action";
 
-import {switchMap, map, catchError} from "rxjs/operators";
-import {of} from "rxjs/observable/of";
-import * as fromServices from '../../services'
+import { switchMap, map, catchError } from "rxjs/operators";
+import { of } from "rxjs/observable/of";
+import * as fromServices from "../../services";
 
 @Injectable()
 export class PizzasEffects {
-
-  constructor(private actions$: Actions, private pizzaService: fromServices.PizzasService) {
-  }
+  constructor(
+    private actions$: Actions,
+    private pizzaService: fromServices.PizzasService
+  ) {}
 
   @Effect()
-  loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS)
-    .pipe(
-      switchMap(() => {
-        return this.pizzaService.getPizzas().pipe(
+  loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS).pipe(
+    switchMap(() => {
+      return this.pizzaService
+        .getPizzas()
+        .pipe(
           map(pizzas => new pizzaActions.LoadPizzasSuccess(pizzas)),
           catchError(error => of(new pizzaActions.LoadPizzasFail(error)))
-        )
-      }));
+        );
+    })
+  );
+
+  @Effect()
+  createPizza$ = this.actions$.ofType(pizzaActions.CREATE_PIZZA).pipe(
+    map((action: pizzaActions.CreatePizza) => action.payload),
+    switchMap(pizza => {
+      return this.pizzaService
+        .createPizza(pizza)
+        .pipe(
+          map(pizza => new pizzaActions.CreatePizzaSuccess(pizza)),
+          catchError(error => of(new pizzaActions.CreatePizzaFail(error)))
+        );
+    })
+  );
 }
