0d1586d569bf95ff78df4cd67f436dbdb3b8960b
Jeroen Druwe <druwe.jeroen@gmail.com>
2018-02-15 08:50:45 +0100

    Added change detection

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -10,6 +10,7 @@ import { tap } from "rxjs/operators/tap";
 
 @Component({
   selector: "product-item",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ["product-item.component.scss"],
   template: `
     <div
